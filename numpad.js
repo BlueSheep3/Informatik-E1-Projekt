a8b5400196115443eddbb67725cbc0c384f88cfb
BlueSheep3 <arne.daude@t-online.de>
2022-11-20 13:40:31 +0100

    numpad page

---
+++ numpad.js
@@ -0,0 +1,34 @@
+"strict mode";
+
+let seed;
+
+window.onload = function() {
+	const urlParams = new URLSearchParams(window.location.search);
+	if(urlParams.get("s") != null)
+		seed = parseInt(urlParams.get("s"));
+}
+
+function onClick(n) {
+	let code = document.getElementById("code");
+	if(code.textContent.length >= 6) return;
+	code.textContent += n;
+}
+
+function onClickDelete() {
+	let code = document.getElementById("code");
+	if(code.textContent.length == 0) return;
+	code.textContent = code.textContent.substring(0, code.textContent.length - 1);
+}
+
+function onClickConfirm() {
+	let code = document.getElementById("code");
+	let correctCode = seed ?? "69420";
+	if(code.textContent == correctCode) {
+		window.location.href = "./testing.html";
+	} else {
+		code.style = "color: #b02020";
+		setTimeout(() => {
+			code.style = "";
+		}, 500);
+	}
+}

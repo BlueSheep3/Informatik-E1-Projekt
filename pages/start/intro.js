92d7927c7825b263fb7441fe8bfd5719d43afa6b
BlueSheep3 <arne.daude@t-online.de>
2022-11-27 21:57:47 +0100

    made intro skippable by pressing esc

--- pages/start/intro.js
+++ pages/start/intro.js
@@ -3,8 +3,9 @@ const urlParams = new URLSearchParams(window.location.search);
 	if(urlParams.get("speed") != null)
 		speed = urlParams.get("speed");
 
+let interval = null;
 if(speed > 0)
-	setInterval(updateText, speed);
+	interval = setInterval(updateText, speed);
 else
 	setTimeout(typeAllText, 5);
 
@@ -13,7 +14,11 @@ function updateText() {
 	let notWritten = document.getElementById("notwritten");
 	let c = notWritten.textContent[0];
 
-	if(notWritten.textContent.length == 0) return;
+	if(notWritten.textContent.length == 0) {
+		if(interval != null) clearInterval(interval);
+		document.getElementById("buttons").classList.remove("hidden");
+		return;
+	}
 
 	written.textContent += c;
 	notWritten.textContent = notWritten.textContent.substring(1);
@@ -22,7 +27,9 @@ function updateText() {
 function typeAllText() {
 	let written = document.getElementById("written");
 	let notWritten = document.getElementById("notwritten");
+	if(notWritten.textContent == "") return;
 
+	document.getElementById("buttons").classList.remove("hidden");
 	written.textContent += notWritten.textContent;
 	notWritten.textContent = "";
 }
@@ -34,4 +41,10 @@ function swapButtons() {
 	else ba.classList.add("swapped");
 	if(bd.classList.contains("swapped")) bd.classList.remove("swapped");
 	else bd.classList.add("swapped");
+}
+
+document.onkeydown = function(e) {
+	if(e.key != "Escape") return;
+	if(interval != null) clearInterval(interval);
+	typeAllText();
 }

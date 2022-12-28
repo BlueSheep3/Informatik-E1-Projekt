3e482d2aa757e52ec828fa58d42f478ebd49b6da
BlueSheep3 <arne.daude@t-online.de>
2022-12-28 12:26:11 +0100

    general achievement system

--- global.js
+++ global.js
@@ -52,3 +52,39 @@ function setHasClass(obj, className, condition) {
 			obj.classList.remove(className);
 	}
 }
+
+function setAchiev(achievName, achievIcon, achievDesc) {
+	if(localStorage.getItem("achievement: " + achievName) != null) return;
+	localStorage.setItem("achievement: " + achievName, "got");
+
+	let div = document.createElement("div");
+	div.classList.add("achiev");
+
+	let table = document.createElement("table");
+	div.appendChild(table);
+	let tr = document.createElement("tr");
+	table.appendChild(tr);
+	let td1 = document.createElement("td");
+	let td2 = document.createElement("td");
+	tr.appendChild(td1);
+	tr.appendChild(td2);
+
+	let img = document.createElement("img");
+	img.src = achievIcon;
+	td1.appendChild(img);
+
+	let title = document.createElement("p");
+	title.id = "title";
+	title.textContent = achievName;
+	let desc = document.createElement("p");
+	desc.id = "desc";
+	desc.textContent = achievDesc;
+	td2.appendChild(title);
+	td2.appendChild(desc);
+
+	document.getElementsByTagName("body")[0].append(div);
+
+	setTimeout(() => div.classList.add("anim"), 1);
+	setTimeout(() => div.classList.remove("anim"), 3000);
+	setTimeout(() => div.remove(), 4000);
+}

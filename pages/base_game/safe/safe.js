bff79e8be6b9dc698e5a09cee1c21d830b122925
BlueSheep3 <arne.daude@t-online.de>
2022-12-28 15:57:11 +0100

    safe

--- pages/base_game/safe/safe.js
+++ pages/base_game/safe/safe.js
@@ -1,15 +1,23 @@
-document.onkeydown = function(e) {
-	if(e.key == " ") openSafe();
+window.onload = function() {
+	if(urlData.act === "f") return;
+	document.getElementById("codehelp").classList.remove("hidden");
+	document.getElementById("mainnumpad").classList.remove("hidden");
+}
+
+window.onclick = function() {
+	if(urlData.act === "f") openSafe();
 }
 
 function openSafe() {
 	document.getElementsByTagName("body")[0].style.backgroundImage
 		= "url('../../../images/backgrounds/safe_open.png')";
 	document.getElementById("doi").classList.remove("hidden");
+	document.getElementById("codehelp").classList.add("hidden");
+	document.getElementById("mainnumpad").classList.add("hidden");
 
 	new Howl({
 		src: ["../../../music/the_holy.mp3"],
-		volume: 0.25,
+		volume: 0.5,
 		loop: true
 	}).play();
 }
@@ -26,4 +34,31 @@ function clickDOI() {
 	setAchiev("Master Thief", "../../../images/decleration_of_independence.png", "Steal the Decleration of Independance");
 
 	setTimeout(() => gotoUrl("../../end/decleration_of_independence/decleration", ''), 5000);
+}
+
+
+function onClick(n) {
+	let code = document.getElementById("code");
+	if(code.textContent.length >= 6) return;
+	code.textContent += n;
+}
+
+function onClickDelete() {
+	let code = document.getElementById("code");
+	if(code.textContent.length == 0) return;
+	code.textContent = code.textContent.substring(0, code.textContent.length - 1);
+}
+
+function onClickConfirm() {
+	let code = document.getElementById("code");
+	// let correctCode = randomInt(urlData.seed, 4386, 10000);
+	let correctCode = 69420; // TODO: change correct code
+	if(code.textContent == correctCode) {
+		openSafe();
+	} else {
+		code.style = "color: #b02020";
+		setTimeout(() => {
+			code.style = "";
+		}, 500);
+	}
 }

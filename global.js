54a776124b54c65b391a7dc0c7b26c75e3bc7a17
BlueSheep3 <arne.daude@t-online.de>
2022-12-28 15:05:09 +0100

    whoops

--- global.js
+++ global.js
@@ -52,8 +52,8 @@ function setHasClass(obj, className, condition) {
 }
 
 function setAchiev(achievName, achievIcon, achievDesc) {
-	// if(localStorage.getItem("achiev: " + achievName) != null) return;
-	// localStorage.setItem("achiev: " + achievName, "got");
+	if(localStorage.getItem("achiev: " + achievName) != null) return;
+	localStorage.setItem("achiev: " + achievName, "got");
 
 	let div = document.createElement("div");
 	div.classList.add("achiev");

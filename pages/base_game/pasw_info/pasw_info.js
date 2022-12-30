2db5907f0bcdbdf982086ec183f5ef9acaf91c75
BlueSheep3 <arne.daude@t-online.de>
2022-12-30 16:11:07 +0100

    password update

--- pages/base_game/pasw_info/pasw_info.js
+++ pages/base_game/pasw_info/pasw_info.js
@@ -2,7 +2,7 @@ window.onload = function() {
 	let hint = document.getElementById("hint");
 	// let rng = randomInt(urlData.seed, 8645, 3);
 	let hintTexts = [
-		"It is one of our most prized possessions\nand the first digit of the master password (without spaces).",
+		"The Password is someone who steals Things\nfollowed by the first digit of our other Password (without spaces).",
 		// "It is one of our most prized possessions\nand the first digit of the master password (without spaces).",
 		// "It is one of our most prized possessions\nand the first digit of the master password (without spaces)."
 	];
